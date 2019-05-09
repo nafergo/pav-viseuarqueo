1ffcba6040c6f55b5b2d5ca2b2e00d313fa4c3ec
nelson gonçalves <nafergo@gmail.com>
2019-05-09 21:45:32 +0100

    Update leaflet-providers.js

--- scripts/leaflet-providers/leaflet-providers.js
+++ scripts/leaflet-providers/leaflet-providers.js
@@ -141,6 +141,14 @@
 				attribution: 'Map data: &copy; <a href="http://www.openseamap.org">OpenSeaMap</a> contributors'
 			}
 		},
+		RomanMap: {
+			url: 'http://pelagios.dme.ait.ac.at/tilesets/imperium//{z}/{x}/{y}.png',
+			options: {
+				maxZoom: 10,
+				attribution: 'Map data: {attribution.OpenStreetMap}, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+			}
+		},
+
 		OpenTopoMap: {
 			url: '//{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
 			options: {
