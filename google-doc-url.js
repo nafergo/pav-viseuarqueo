79219c0a3b2779e15eabc5862c1127eca1e8593c
nelson gonçalves <nafergo@gmail.com>
2019-07-08 17:57:14 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,3 +1,3 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1bUYF_DNd-6-xWsVJIAvZwaR4Nc76XIdn-0B6nnY1VKE/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/19y5XyuRwL3sN5j-8F9lNmwnQoasYr6BOTLRvrFUMQgU/edit#gid=0';
 
 
